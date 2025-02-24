667bf7b7f5a1109ffd299e630415d7c2f4f9dd37
missionbells <eriksigbladh@gmail.com>
2025-02-23 22:44:41 -0500

    Cleaned up UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,13 @@
 "use client"; // Ensures client-side rendering
 
 import ParticleWave from "@/components/ParticleWave";
-import LoginCard from "@/components/LoginCard";
 import Image from "next/image";
 
 export default function Home() {
 
   return (
     <div className="relative min-h-screen">
-      {/* Logo - Always Visible */}
-      <div className="absolute top-6 left-1/2 transform -translate-x-1/2 z-20">
-        <Image
-          src="/SolaceLogo.png" // Ensure this file is inside the `public/` folder
-          alt="Solace Logo"
-          width={150} // Adjust size if needed
-          height={50}
-          className="opacity-90"
-        />
-      </div>
-
-
-        <>
           <ParticleWave />
-          <LoginCard />
-        </>
     </div>
   );
 }
